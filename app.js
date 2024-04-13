a26c6aaa2958360bd79d1b8ad608b6a783633162
chidubem100 <dubemernest23@gmail.com>
2024-04-13 22:47:44 +0100

    bug fixes

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const socketio = require("socket.io");
 const run = require("./seedDB");
 const jwt = require("jsonwebtoken");
 const app = express();
-const allowedOrigins = [
-    'hhtps://neo-protocol.com',    
+const allowedOrigins = [  
+    "https://neo-protocol.com",  
     'http://localhost:8081',
     'http://localhost:7070',
     'http://localhost:4040',
